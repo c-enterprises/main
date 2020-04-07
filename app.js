d0c3198f0306a457b666d630ce83507160097c2a
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-04-07 22:35:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+window.onload =setInterval(() => {
+    const loader = document.querySelector(".loader");
+    const body = document.querySelector("body");
+    const main = document.querySelector(".main");
+    loader.style.display = "none";
+
+    body.style.backgroundColor = "#000000"
+   
+
+    main.style.display = "block";
+    
+}, 15000);
+
+
+
+
